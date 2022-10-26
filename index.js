49e51b9779753b0bab6de62183ae7828e383f8cc
Jonphia Hart <soldiergurl4life@gmail.com>
2022-10-26 14:11:08 -0400

    updated index.html

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header')
+header.style.textAlign = 'left';
+header.textContent = 'My Doggos';
+
+
+
+let dogImages = document.querySelectorAll('.dog-image');
+console.log(dogImages)
+
+for (let i = 0; i < dogImages.length; i++) {
+    const element = dogImages[i];
+    console.log(element)
+    element.style.borderRadius = '50px'
+    element.style.border = '5px solid red';
+}
+
+
+let footer = document.querySelector('.footer');
+footer.styke.color = 'red';
+
